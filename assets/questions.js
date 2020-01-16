bbb1ecf59851670219f4ad0752647fb35a329ece
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-16 00:33:42 -0800

    complete pseudocode for questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -2,12 +2,66 @@ const queries = require("queries.js");
 const inquirer = require("inquirer");
 
 //SET UP QUESTIONS/PROMPTS FOR USER IN CLI
-
+//------------------------------------------------------------------------
+//INITIAL QUESTION
+//
 //What would you like to do?
 //"View", "Add", "Update Employee Info"
 
-//update employee info - Please select a catagory
+//Please select a catagory
 //"Department(s)", "Role(s)", "Employee(s)"
 
+//------------------------------------------------------------------------
+//ADD DEPARTMENT
+//
+//What is the name of the department?
+
+//------------------------------------------------------------------------
+//ADD ROLE
+//
+// What is the title of the role?
+
+// What is the salary of the role?
+
+//Which department does the role belong to?
+
+//------------------------------------------------------------------------
+//ADD EMPLOYEE
+//What is the employee's first name?
+
+//What is the employee's last name?
+
+//What is the employee's job title?
+
+//Who is the employee's direct manager?
+
+//------------------------------------------------------------------------
+//UPDATE EMPLOYEE
+//
+//Which employee would you like to update?
+
+//What do you want to update?
+
+//Select the employee's new manager
+
+//Select the employee's new role
+
+//------------------------------------------------------------------------
+//SORT STAFF
+//
+
+
+//------------------------------------------------------------------------
+//RUN
+//
+
+
+//------------------------------------------------------------------------
+//
+//
+
+
+//------------------------------------------------------------------------
+//
 //
 
